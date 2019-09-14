7fb9ff3469c3c8f9acbe7c9ada3c4717a31b2bdd
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2019-09-14 16:35:30 +0100

    Remove logger dependency (#11)
    * removed nlog dependency and added ILogger abstraction
    
    * nuget update 1.0.2

---
+++ src/TestClient/NLogLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NLog;
+using ILogger = Linker.ILogger;
+
+namespace TestClient
+{
+    public class NLogLogger : ILogger
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public void Info(string message)
+        {
+            Log.Info(message);
+        }
+
+        public void Warn(string message)
+        {
+            Log.Warn(message);
+        }
+
+        public void Warn(string message, Exception ex)
+        {
+            Log.Warn(message, ex.GetBaseException().Message);
+        }
+
+        public void Error(string message)
+        {
+            Log.Error(message);
+        }
+
+        public void Error(string message, Exception ex)
+        {
+            Log.Error(message, ex.GetBaseException().Message);
+        }
+
+        public void Debug(string message)
+        {
+            Log.Debug(message);
+        }
+    }
+}
