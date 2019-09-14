237b5f3a6188242dd390ce4cbda0d40064d03267
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2019-09-14 16:35:30 +0100

    Remove logger dependency (#11)
    * removed nlog dependency and added ILogger abstraction
    
    * nuget update 1.0.2

---
+++ src/Linker/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Linker
+{
+    public interface ILogger
+    {
+        void Info(string message);
+        void Warn(string message);
+        void Warn(string message, Exception ex);
+        void Error(string message);
+        void Error(string message, Exception ex);
+        void Debug(string message);
+    }
+}
