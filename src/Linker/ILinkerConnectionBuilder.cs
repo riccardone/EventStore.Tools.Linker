f93fcf0cdd64e77f2aac007ffe9be0906dd40bd5
Piotr Błachut <blachut.piotr@gmail.com>
2019-10-12 12:09:01 +0200

    replace ConnectionBuilder with BuildConnection delegate (#14)

--- src/Linker/ILinkerConnectionBuilder.cs
+++ src/Linker/ILinkerConnectionBuilder.cs
@@ -5,7 +5,9 @@ namespace Linker
 {
     public interface ILinkerConnectionBuilder
     {
+        [Obsolete]
         Uri ConnectionString { get; }
+        [Obsolete]
         ConnectionSettings ConnectionSettings { get; }
         string ConnectionName { get; }
         IEventStoreConnection Build();
