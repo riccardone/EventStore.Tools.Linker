ea365756673c74ecbe85d53d9c04ede309d62e9d
Riccardo Di Nuzzo <riccardo.d@xml-int.com>
2025-06-09 08:40:26 +0100

    add cancellation token to subscribet event handling

--- src/Linker/LinkerService.cs
+++ src/Linker/LinkerService.cs
@@ -171,7 +171,7 @@ public class LinkerService : ILinkerService, IAsyncDisposable
             throw new Exception("Origin connection is not initialized");
         await using var subscription = _originConnection.SubscribeToAll(FromAll.After(_lastPosition), cancellationToken: ctsToken, 
             resolveLinkTos: false, filterOptions: new SubscriptionFilterOptions(EventTypeFilter.ExcludeSystemEvents()));
-        await foreach (var message in subscription.Messages)
+        await foreach (var message in subscription.Messages.WithCancellation(ctsToken))
         {
             switch (message)
             {
